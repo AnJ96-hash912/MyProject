d5030c751a260f5373b3b3c856e899814397b32c
Anupama Jayaraman <tsnippetblog@gmail.com>
2025-01-19 15:36:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from PIL import Image
 caption=pipeline('image-to-text',model='ydshieh/vit-gpt2-coco-en')
 uploaded_image=st.file_uploader("upload an image", type=["png","jpg","jpeg"])
 if uploaded_image is not None:
-    Image=Image.open(uploaded_image)
-    st.image(Image, caption="uploaded_image", use_column_width=True)
+    image=Image.open(uploaded_image)
+    st.image(image, caption="uploaded_image", use_column_width=True)
     if st.button("Generate caption"):
-        captions=caption(Image)
+        captions=caption(image)
         st.write(captions[0],['generated text'])
