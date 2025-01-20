0e08160c429116fb5a164aa984fdd1d2e36b7403
Anupama Jayaraman <tsnippetblog@gmail.com>
2025-01-20 15:58:54 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,10 +1,20 @@
+import streamlit as st
+from transformers import pipeline
+from PIL import Image
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    caption=pipeline('image-to-text',model='ydshieh/vit-gpt2-coco-en')
+    uploaded_image=st.file_uploader("upload an image", type=["png","jpg","jpeg"])
+    if uploaded_image is not None:
+        image=Image.open(uploaded_image)
+        st.image(image, caption="uploaded_image", use_column_width=True)
+        if st.button("Generate caption"):
+            captions=caption(image)
+            st.write(captions[0],['generated text'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
